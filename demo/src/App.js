a89899c5a5de546b645156f463a33753c0a3770b
Devin Sweeting <devin@ridereportapp.com>
2022-05-26 13:08:34 -0500

    move package.json up to root, install typescript, and refactor to rollup

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,15 +1,11 @@
 import "./App.css";
-import Button from "ride-ui-kit/Button";
-import Input from "ride-ui-kit/Input";
-
-import "ride-ui-kit/theme.css";
+import { Button } from "ride-ui-kit";
 
 function App() {
   return (
     <>
       <h1>Ride Report's Components</h1>
-      <Input />
-      <Button>Button component</Button>
+      <Button label={"omg it worked"} />
     </>
   );
 }
--- src/Input.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Input = (props) => {
-  return <input {...props} className="ride-input" />;
-};
-
-export default Input;
