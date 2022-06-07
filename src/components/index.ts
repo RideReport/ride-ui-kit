86423c7a0267f54a726fdf50747f3ba6dad0e49d
Devin Sweeting <devin@ridereportapp.com>
2022-06-07 11:09:10 -0500

    add button props as export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
-export { default as Button } from "./Button/Button";
+export { default as Button, ButtonProps } from "./Button/Button";
 export { default as Icon, IconName } from "./Icons/Icon";
 export { default as Toggle } from "./Toggles/Toggle";
 export { default as Checkbox } from "./Toggles/Checkbox";
